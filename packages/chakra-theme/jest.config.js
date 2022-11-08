bf8e25e7ee36681fa110aa1861d8c15ed07e91a1
Robert Watt <rob.watt@sitecore.com>
2022-11-08 15:45:59 -0600

    Add tests for theme package
    These tests will run on `npx lerna run test`. Hoping we can get a detailed report of each package test run available for further insights on github

---
+++ packages/chakra-theme/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+    moduleFileExtensions: ['js', 'ts'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    }
+};
---
+++ packages/chakra-theme/tests/theme.spec.ts
@@ -0,0 +1,18 @@
+import { ChakraTheme, isChakraTheme } from '@chakra-ui/react';
+import theme from '../src';
+
+describe("Theme", () => {
+  it("should be of type ChakraTheme", () => {
+    // Check if default theme is of type ChakraTheme
+    const defaultThemeIsAChakraTheme: ChakraTheme = theme
+    expect(defaultThemeIsAChakraTheme).toBeTruthy()
+  })
+
+  it("should be check that this is a ChakraTheme", () => {
+    expect(isChakraTheme(theme)).toBeTruthy()
+  })
+
+  it("should be check that this is not a ChakraTheme", () => {
+    expect(isChakraTheme({ colors: {} })).toBeFalsy()
+  })
+})
